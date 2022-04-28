b23805da21b6516fef741e70c6414b9a17cf82fe
ogamespec <ogamespec@gmail.com>
2022-04-28 20:24:34 +0300

    crammap

--- CRAMMap.cpp
+++ CRAMMap.cpp
@@ -1,9 +1,100 @@
-// CRAMMap.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// CRAMMap.cpp: A simple utility for generating PPUs Color RAM (palette) memory cells layout.
 
 #include "pch.h"
 
+const size_t originOffset = 10ULL;
+const size_t cellSize = 10ULL;
+const size_t GapBetweenCells = 1ULL;
+
+void EmitCellText(std::string& text, size_t bit_num, size_t row, size_t col, size_t pp[8])
+{
+    char cell[0x400]{};
+    const char* CellColor = "";
+    char CellLabel[0x20]{};
+
+    size_t PAL4 = (row >> 2) & 1;
+
+    size_t cram_addr = (col << 2) | (row & 3) | (PAL4 << 4);
+
+    if (row == 0)
+    {
+        // Rows 0 and 4 are combined into one row.
+        // (Addresses $3F10/$3F14/$3F18/$3F1C are mirrors of $3F00/$3F04/$3F08/$3F0C)
+
+        CellColor = "Tomato";
+
+        size_t cram_addr2 = (col << 2) | (1 << 4);
+        sprintf_s(CellLabel, sizeof(CellLabel), "%02X/%02X-%zd\nrow: 0/4\ncol: %zd", (uint8_t)cram_addr, (uint8_t)cram_addr2, bit_num, col);
+    }
+    else
+    {
+        CellColor = PAL4 ? "GreenYellow" : "Teal";
+        sprintf_s(CellLabel, sizeof(CellLabel), "%02X-%zd\nrow: %zd\ncol: %zd", (uint8_t)cram_addr, bit_num, row, col);
+    }
+
+    sprintf_s(cell, sizeof(cell), cellText, CellLabel, CellColor,
+        pp[0], pp[1],
+        pp[2], pp[3],
+        pp[4], pp[5],
+        pp[6], pp[7]);
+
+    text += cell;
+}
+
+void GenRowLane(std::string& text, size_t& y, size_t row)
+{
+    size_t pp[8]{};
+
+    for (size_t n = 0; n < 4 * 6; n++)
+    {
+        size_t col = n;
+
+        size_t x = (cellSize + GapBetweenCells) * n + originOffset;
+        size_t cell_y = y;
+
+        pp[0] = x;
+        pp[1] = cell_y;
+        pp[2] = x + cellSize;
+        pp[3] = cell_y;
+        pp[4] = x + cellSize;
+        pp[5] = cell_y + cellSize;
+        pp[6] = x;
+        pp[7] = cell_y + cellSize;
+
+        EmitCellText(text, n / 4, row, col & 3, pp);
+    }
+
+    y += cellSize + GapBetweenCells;
+}
+
+void GenMap()
+{
+    // Order of rows
+    int rows_order[] = { 0, 6, 2, 5, 1, 7, 3 };
+
+    std::string text = "";
+
+    text += headerText;
+
+    size_t y = originOffset;
+
+    for (size_t p = 0; p < _countof(rows_order); p++)
+    {
+        GenRowLane(text, y, rows_order[p]);
+    }
+
+    text += footerText;
+
+    FILE* f;
+    fopen_s(&f, "crammap.xml", "wt");
+    if (f)
+    {
+        fwrite(text.c_str(), 1, text.size(), f);
+        fclose(f);
+    }
+}
+
 int main()
 {
-
+    GenMap();
 }
--- Readme.md
+++ Readme.md
@@ -4,6 +4,21 @@ A simple utility for generating PPUs Color RAM (palette) memory cells layout.
 
 The map is presented as a set of squares, each square corresponds to one physical location of a memory cell on the chip.
 
+Keep in mind that Color RAM is designed so that 0x0/0x4/0x8/0xC and 0x10/0x14/0x18/0x1C addresses point to the same memory cells (Mirroring).
+
+Color differentiation of memory cells:
+- Tomato: Cells for which Mirroring is applied (same colors for background/sprite)
+- GreenYellow: Cells for sprite color (CRAM Address msb = 1)
+- Teal: Cells for the color of the background (CRAM Address msb = 0)
+
+Inside each cell is a caption, of the form `AA-B`:
+- AA: Cell address 00-FF
+- B: The bit number of the corresponding byte.
+
+Here's what it turns out:
+
+![crammap.png](crammap.png)
+
 ## How to view the map
 
 The utility creates an .xml file, which should be opened in the Deroute utility (File -> Add entities...)
